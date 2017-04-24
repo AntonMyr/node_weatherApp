a3c7355d156efdaf3eebd8417743b6eb2de5a13b
antonmyr <15anmy@stockholmscience.se>
2017-04-24 10:28:21 +0200

    remade it with promises instead of callbacks

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
 #!/usr/bin/env node
-const yargs = require('yargs');
+const yargs = require("yargs");
 
-const geocode = require('./geocode/geocode');
-const weather = require('./weather/weather');
+const geocode = require("./geocode/geocode");
+const weather = require("./weather/weather");
 
 const argv = yargs
   .options({
     a: {
       demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
+      alias: "address",
+      describe: "Address to fetch weather for",
       string: true
     }
   })
   .help()
-  .alias('help', 'h')
-  .argv;
+  .alias("help", "h").argv;
 
-geocode.geocodeAddress(argv.a, (errorMessage, results) => {
-  if (errorMessage) {
-    console.log(errorMessage);
-  } else {
+geocode.geocodeAddress(argv.a).then(
+  results => {
     console.log(results.address);
-    weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
-      if (errorMessage) {
+    weather.getWeather(results.latitude, results.longitude).then(
+      weatherResults => {
+        console.log(
+          `It's currently ${weatherResults.currentTemperature}. It feels like ${weatherResults.apparentTemperature}. \nWeather summary: ${weatherResults.weatherSummary}`
+        );
+      },
+      errorMessage => {
         console.log(errorMessage);
-      } else {
-        console.log(`It's currently ${weatherResults.currentTemperature}. It feels like ${weatherResults.apparentTemperature}. \nWeather summary: ${weatherResults.weatherSummary}`);
       }
-    });
+    );
+  },
+  errorMessage => {
+    console.log(errorMessage);
   }
-});
+);
